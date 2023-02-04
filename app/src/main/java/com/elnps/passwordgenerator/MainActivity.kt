9a2e15279bfeab6b4c7b117b9237a12d7f37eadc
Eismer Lobaina <113050158+eismerelnps@users.noreply.github.com>
2023-02-04 17:44:34 -0500

    v1.0

--- app/src/main/java/com/elnps/passwordgenerator/MainActivity.kt
+++ app/src/main/java/com/elnps/passwordgenerator/MainActivity.kt
@@ -1,49 +1,57 @@
 package com.elnps.passwordgenerator
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.RadioButton
-import android.widget.TextView
+import android.widget.*
+import androidx.annotation.RequiresApi
 
 class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val generateButton: Button = findViewById(R.id.generateButton)
+        val passwordLengthSeekBar: SeekBar = findViewById(R.id.passwordLengthSeekBar)
+        val copyToClipboardButton: Button = findViewById(R.id.copyToClipboardButton)
+        passwordLengthSeekBar.max = 120
+        passwordLengthSeekBar.min = 4
+        passwordLengthSeekBar.progress = 8
         generateButton.setOnClickListener{checkValue()}
+        copyToClipboardButton.setOnClickListener { copyToClipboard() }
     }
 
 
 
     fun checkValue() {
         val passwordTextView: TextView = findViewById(R.id.passwordTextView)
-        val passwordLengthEditText: EditText = findViewById(R.id.passwordLengthEditText)
         val CapitalRadioButton: RadioButton = findViewById(R.id.CapitalRadioButton)
         val minRadioButton: RadioButton = findViewById(R.id.minRadioButton)
         val numberRadioButton: RadioButton = findViewById(R.id.numberRadioButton)
         val specialCharRadioButton: RadioButton = findViewById(R.id.specialCharRadioButton)
+        val passwordLengthSeekBar: SeekBar = findViewById(R.id.passwordLengthSeekBar)
         val generator = Generator()
-        if (passwordLengthEditText.text.isEmpty()) {
-            passwordTextView.text = "Please set a length first"
-        } else if (passwordLengthEditText.text.toString()
-                .toInt() <= 4 || passwordLengthEditText.text.toString().toInt() > 120
-        ) {
-            passwordTextView.text = "For security set a length between 4 and 120 characters "
-        } else if (!CapitalRadioButton.isChecked && !minRadioButton.isChecked && !numberRadioButton.isChecked && !specialCharRadioButton.isChecked) {
+
+       if (!CapitalRadioButton.isChecked && !minRadioButton.isChecked && !numberRadioButton.isChecked && !specialCharRadioButton.isChecked) {
             passwordTextView.text = "Set at least one of the requirements"
         } else {
             generator.setOptions(
-                passwordLengthEditText.text.toString().toInt(),
+                passwordLengthSeekBar.progress.toString().toInt(),
                 CapitalRadioButton.isChecked,
                 minRadioButton.isChecked,
                 numberRadioButton.isChecked,
                 specialCharRadioButton.isChecked
             )
-            val password: String
-            password = generator.generate()
             passwordTextView.text = generator.generate()
         }
+    }
+    fun copyToClipboard(){
+        val passwordTextView: TextView = findViewById(R.id.passwordTextView)
+        val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        val clip = ClipData.newPlainText("Password", passwordTextView.text.toString())
+        clipboard.setPrimaryClip(clip)
     }
 }
